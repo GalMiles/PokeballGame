bfb8f14320455a02ed0e56334648ef071f7f446e
GalMiles <58370322+GalMiles@users.noreply.github.com>
2021-11-23 12:49:12 +0200

    add buttons and funcuality in script

--- JS/script1.js
+++ JS/script1.js
@@ -1,39 +1,51 @@
-let dx=2;
-let dy = -2;
-let BALLRADIUS =20;
-let is_close=true;
-let POKEBALLS_AMOUNT = 4;
-let id;
-let inGame = false;
-const pokeballArray =[];
-
+//global vairable
+let g_state = {};
+g_state.dx=2;
+g_state.dy = -2;
+g_state.BALLRADIUS =20;
+g_state.is_close=true;
+g_state.POKEBALLS_AMOUNT = 4;
+g_state.id;
+g_state.inGame = false;
+g_state.pokeballArray =[];
+g_state.timer_id = null;
+g_state.is_timer_mode = true; //check if we in timer or not
+ 
 
 //get the canvas element
-let canvas=document.getElementById("responsive-canvas");
+g_state.canvas=document.getElementById("responsive-canvas");
 // call the getContext mathod to draw 2d shape
-const context = canvas.getContext("2d");
-let img = document.querySelector("img");
+g_state.context = g_state.canvas.getContext("2d");
+g_state.img = document.querySelector("img");
+
+g_state.btn_start = document.querySelector( '#btn_start');
+g_state.btn_start.addEventListener( 'click', handle_start);
+		
+g_state.btn_pause = document.querySelector( '#btn_pause');
+g_state.btn_pause.addEventListener( 'click', handle_pause);
+
+g_state.btn_reset = document.querySelector( '#btn_reset');
+g_state.btn_reset.addEventListener( 'click', handle_reset);
+
+g_state.timer = document.querySelector( '#ftime');
+g_state.timer_value = g_state.timer.value; //getting timer value
 
 //set image for the balls
-let img_close1= new Image();
-let img_close2= new Image();
-let img_close3= new Image();
-let img_close4= new Image();
-//let img_open = new Image();
+g_state.img_close1= new Image();
+g_state.img_close2= new Image();
+g_state.img_close3= new Image();
+g_state.img_close4= new Image();
 
-img_close1.src = 'Images/closePokeball.png';
-img_close2.src = 'Images/closePokeball.png';
-img_close3.src = 'Images/closePokeball.png';
-img_close4.src = 'Images/closePokeball.png';
-//img_open.src = 'Images/openPokeball.png';
+g_state.img_close1.src = 'Images/closePokeball.png';
+g_state.img_close2.src = 'Images/closePokeball.png';
+g_state.img_close3.src = 'Images/closePokeball.png';
+g_state.img_close4.src = 'Images/closePokeball.png';
 
-img_close1.width=img_close1.height=BALLRADIUS;
-img_close2.width=img_close2.height=BALLRADIUS;
-img_close3.width=img_close3.height=BALLRADIUS;
-img_close4.width=img_close4.height=BALLRADIUS;
+g_state.img_close1.width=g_state.img_close1.height=g_state.BALLRADIUS;
+g_state.img_close2.width=g_state.img_close2.height=g_state.BALLRADIUS;
+g_state.img_close3.width=g_state.img_close3.height=g_state.BALLRADIUS;
+g_state.img_close4.width=g_state.img_close4.height=g_state.BALLRADIUS;
 
-// img_open.width=BALLRADIUS;
-// img_open.height=BALLRADIUS;
 
 //create ball class
 class Pokeball
@@ -46,7 +58,7 @@ class Pokeball
         this.vely = vely; //velocity y added to coordinate x when we animate our ball
         this.img = imgPoke; //set pokeball image 
         this.exploded= false;
-        this.size = BALLRADIUS;
+        this.size = g_state.BALLRADIUS;
         this.initSize = this.size;
         this.explosionSize = 10;
         this.color="white";
@@ -54,10 +66,10 @@ class Pokeball
 
     draw()
     {
-            context.beginPath();
-            context.drawImage(this.img,this.x,this.y, this.img.width, this.img.height);
-            context.fill(); //finish drawing
-            context.closePath();
+        g_state.context.beginPath();
+        g_state.context.drawImage(this.img,this.x,this.y, this.img.width, this.img.height);
+        g_state.context.fill(); //finish drawing
+        g_state.context.closePath();
     }
 
     moving()
@@ -67,9 +79,9 @@ class Pokeball
             this.x += this.velx;
             this.y +=this.vely;
             //if x and y position is greater or less than canvas than turn to another direction
-            if(this.x  >= canvas.width-BALLRADIUS || this.x  <= 0) 
+            if(this.x  >= g_state.canvas.width-g_state.BALLRADIUS || this.x  <= 0) 
                 this.velx = -this.velx;
-            if(this.y  >= canvas.height-BALLRADIUS  || this.y  <= 0) 
+            if(this.y  >= g_state.canvas.height-g_state.BALLRADIUS  || this.y  <= 0) 
                 this.vely = -this.vely;
         }
         else
@@ -84,7 +96,7 @@ class Pokeball
             else 
             {
                 this.velx=this.vely=0;
-                pokeballArray.splice(pokeballArray.indexOf(this), 1);
+                g_state.pokeballArray.splice(g_state.pokeballArray.indexOf(this), 1);
                 
             }
         }
@@ -121,11 +133,11 @@ class Pokeball
 
 function show_pokeballs()
 {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for(let i=0; i< pokeballArray.length; i++)
+    g_state.context.clearRect(0, 0, g_state.canvas.width, g_state.canvas.height);
+    for(let i=0; i< g_state.pokeballArray.length; i++)
     {
-        pokeballArray[i].moving();
-        pokeballArray[i].draw();
+        g_state.pokeballArray[i].moving();
+        g_state.pokeballArray[i].draw();
     }
 }
 
@@ -133,36 +145,36 @@ function loop(){
     show_pokeballs();
     let i=0;
     let j=1;
-    while (i< pokeballArray.length)
+    while (i< g_state.pokeballArray.length)
     {
-        while(j < pokeballArray.length && !pokeballArray[i].exploded )
+        while(j < g_state.pokeballArray.length && !g_state.pokeballArray[i].exploded )
         {
-            let thisball = pokeballArray[j];
-            if(pokeballArray[i].x > thisball.x && pokeballArray[i].x < thisball.x + BALLRADIUS && pokeballArray[i].y > thisball.y && pokeballArray[i].y < thisball.y + BALLRADIUS)
+            let thisball = g_state.pokeballArray[j];
+            if(g_state.pokeballArray[i].x > thisball.x && g_state.pokeballArray[i].x < thisball.x + g_state.BALLRADIUS && g_state.pokeballArray[i].y > thisball.y && g_state.pokeballArray[i].y < thisball.y + g_state.BALLRADIUS)
             {
                     //there is hit between two pokeballs
                     //the slower will explode and the faster will change diretion
                     
-                if(Math.abs(pokeballArray[i].velx)+Math.abs(pokeballArray[i].vely) < Math.abs(pokeballArray[j].velx)+Math.abs(pokeballArray[j].vely))
+                if(Math.abs(g_state.pokeballArray[i].velx)+Math.abs(g_state.pokeballArray[i].vely) < Math.abs(g_state.pokeballArray[j].velx)+Math.abs(g_state.pokeballArray[j].vely))
                 {
-                    pokeballArray[i].exploded=true;
+                    g_state.pokeballArray[i].exploded=true;
                     show_pokeballs();
-                    context.beginPath();
-                    context.arc(pokeballArray[i].x, pokeballArray[i].y, pokeballArray[i].explosionSize, 0, Math.PI * 2);
-                    context.strokeStyle = pokeballArray[i].color;
-                    context.stroke();
-                    context.closePath();
+                    g_state.context.beginPath();
+                    g_state.context.arc(g_state.pokeballArray[i].x, g_state.pokeballArray[i].y, g_state.pokeballArray[i].explosionSize, 0, Math.PI * 2);
+                    g_state.context.strokeStyle = pokeballArray[i].color;
+                    g_state.context.stroke();
+                    g_state.context.closePath();
                     //pokeballArray.pop(pokeballArray[i]); //remove the ball from the array
                 }
                 else
                 {
-                    pokeballArray[j].exploded=true;
+                    g_state.pokeballArray[j].exploded=true;
                     show_pokeballs();
-                      context.beginPath();
-                      context.arc(pokeballArray[j].x, pokeballArray[j].y, pokeballArray[j].explosionSize, 0, Math.PI * 2);
-                      context.strokeStyle = pokeballArray[j].color;
-                      context.stroke();
-                      context.closePath();
+                    g_state.context.beginPath();
+                    g_state.context.arc(g_state.pokeballArray[j].x, g_state.pokeballArray[j].y, g_state.pokeballArray[j].explosionSize, 0, Math.PI * 2);
+                    g_state.context.strokeStyle = g_state.pokeballArray[j].color;
+                    g_state.context.stroke();
+                    g_state.context.closePath();
                     //pokeballArray.pop(pokeballArray[j]); //remove the ball from the array
                 }
             }
@@ -180,31 +192,34 @@ function loop(){
  }
 
 function init() {
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    g_state.context.fillRect(0, 0, g_state.canvas.width, g_state.canvas.height);
     const pokeball1 = new Pokeball(
-        random(BALLRADIUS, canvas.width - BALLRADIUS), 
-        random(BALLRADIUS, canvas.height- BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.width - g_state.BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.height- g_state.BALLRADIUS), 
         random(-2,2), 
-        random(2,2), img_close1);
-    pokeballArray.push(pokeball1);
+        random(2,2), g_state.img_close1);
+    g_state.pokeballArray.push(pokeball1);
+
     const pokeball2 = new Pokeball(
-        random(BALLRADIUS, canvas.width - BALLRADIUS), 
-        random(BALLRADIUS, canvas.height- BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.width - g_state.BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.height- g_state.BALLRADIUS), 
         random(-2,2), 
-        random(2,2), img_close2);
-    pokeballArray.push(pokeball2);
+        random(2,2), g_state.img_close2);
+    g_state.pokeballArray.push(pokeball2);
+
     const pokeball3 = new Pokeball(
-        random(BALLRADIUS, canvas.width - BALLRADIUS), 
-        random(BALLRADIUS, canvas.height- BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.width - g_state.BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.height- g_state.BALLRADIUS), 
         random(-2,2), 
-        random(2,2), img_close3);
-    pokeballArray.push(pokeball3);
+        random(2,2), g_state.img_close3);
+    g_state.pokeballArray.push(pokeball3);
+
     const pokeball4 = new Pokeball(
-        random(BALLRADIUS, canvas.width - BALLRADIUS), 
-        random(BALLRADIUS, canvas.height- BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.width - g_state.BALLRADIUS), 
+        random(g_state.BALLRADIUS, g_state.canvas.height- g_state.BALLRADIUS), 
         random(-2,2), 
-        random(2,2), img_close4);
-    pokeballArray.push(pokeball4);
+        random(2,2), g_state.img_close4);
+    g_state.pokeballArray.push(pokeball4);
 
 
 
@@ -218,23 +233,67 @@ function init() {
     //     random(2,2));
     //     pokeballArray.push(pokeball);
     // }
-    inGame = true;
+    g_state.inGame = true;
     anim();
 }
 
 function anim() 
 {
-  if (inGame) 
+  if (g_state.inGame) 
     window.requestAnimationFrame(anim);
   loop();
-  if (pokeballArray.length === 1) //ifirst problem need to fix
-    gameOver();
+  if (g_state.pokeballArray.length === 1 || !g_state.is_timer_mode) 
+    game_over();
 }
-function gameOver() {
-  inGame = false;
+function game_over() {
+    g_state.inGame = false;
+    g_state.is_timer_mode = false;
+    clearInterval(g_state.timer_id);
 }
 
-init();
+function handle_start()
+{
+    if ( !g_state.timer_id ){
+
+        g_state.timer_value = g_state.timer.value; //getting timer value from input
+        init();
+        g_state.timer_id = window.setInterval(  handle_tick, 1000 )  
+    }
+    g_state.is_timer_mode = true;
+}
+
+function handle_pause()
+{
+    if(!g_state.timer_id) return;
+
+    g_state.is_timer_mode = false
+    //clear canvas;
+}
+
+function handle_reset()
+{
+    if(!g_state.timer_id || g_state.is_timer_mode ) return;
+
+    g_state.context.clearRect(0, 0, g_state.canvas.width, g_state.canvas.height);
+    g_state.is_timer_mode = false;
+    clearInterval(g_state.timer_id);
+    g_state.timer_id =  null;
+    // handle_start();
+}
+
+//decending timer value
+function handle_tick()
+{
+	if ( !g_state.timer_value) {
+        game_over();
+    }
+
+    else{
+        g_state.timer_value--;
+    }
+}
+
+
 
 
  
